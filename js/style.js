1d0da4c0727442bea88b420f1ffc6a75d888dd97
catalinbroinas <catalinbroinas@yahoo.com>
2023-11-04 12:43:59 +0200

    Add necessary folders, files and basic functions JS functions: add, subtract, multiply and divide.

---
+++ js/style.js
@@ -0,0 +1,49 @@
+function add(num1, num2)
+{
+    return parseInt(num1) + parseInt(num2);
+}
+
+function subtract(num1, num2)
+{
+    return parseInt(num1) - parseInt(num2);
+}
+
+function multiply(num1, num2)
+{
+    return parseInt(num1) * parseInt(num2);
+}
+
+function divide(num1, num2)
+{
+    return parseInt(num1) / parseInt(num2);
+}
+
+function operate(operate, num1, num2)
+{
+    switch(operate)
+    {
+        case 'add':
+            return add(num1, num2);
+            break;
+        case 'subtract':
+            return subtract(num1, num2);
+            break;
+        case 'multiply':
+            return multiply(num1, num2);
+            break;
+        case 'divide':
+            return divide(num1, num2);
+            break;
+        default:
+            return 'This operate does not exist in this application';
+    }
+}
+
+let num1 = prompt('Introduce the first number.');
+let num2 = prompt('Introduce the second number.');
+
+const container = document.querySelector('.container');
+const test = document.createElement('p');
+test.classList.add('text');
+test.textContent = `Result of the add operation is ${operate('divide', num1, num2)}`;
+container.appendChild(test);
