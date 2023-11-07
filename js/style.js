5b126fba385fb9061548612fb2147832a86dd90b
catalinbroinas <catalinbroinas@yahoo.com>
2023-11-07 18:22:16 +0200

    Fix: replace setNum1() with setNumber() Add calculator object, add function for display numbers and operator

--- js/style.js
+++ js/style.js
@@ -1,3 +1,11 @@
+const numbers = document.querySelectorAll('.numeric');
+const operators = document.querySelectorAll('.operator');
+const calculator = {
+    num1: null,
+    num2: null,
+    operate: null
+};
+
 function add(num1, num2)
 {
     return parseInt(num1) + parseInt(num2);
@@ -18,35 +26,47 @@ function divide(num1, num2)
     return parseInt(num1) / parseInt(num2);
 }
 
-function setNum1(value)
+function setNumber(value)
 {
     const calcDisplay = document.querySelector('#calculator-display'); 
     let initValue = Number.parseFloat(calcDisplay.textContent);
+    
 
     if(initValue === 0)
     {
         calcDisplay.textContent = value;    
     }
+    else if(!Number.isInteger(initValue))
+    {
+        calcDisplay.textContent = value;
+    }
     else
     {
-        calcDisplay.textContent = initValue + value;
+       calcDisplay.textContent = initValue + value;
     }
     
     return Number.parseFloat(calcDisplay.textContent);
 }
 
-function getNum1()
+function getNumber(value)
 {
-    const calcDisplay = document.querySelector('#calculator-display');
-    const numValue = calcDisplay.textContent;
-
-    return Number.parseFloat(numValue);
+    if(calculator['num1'] === null)
+    {
+        calculator['num1'] = value;
+    }
+    else
+    {
+        calculator['num2'] = value;    
+    } 
 }
 
 function setOperation(value)
 {
+    const calcDisplay = document.querySelector('#calculator-display'); 
     let operator = value;
-    
+    calculator['operate'] = value;
+    calcDisplay.textContent = value;
+       
     return operator;
 }
 
@@ -71,17 +91,23 @@ function operate(operate, num1, num2)
     }
 }
 
-const numbers = document.querySelectorAll('.numeric');
-const operators = document.querySelectorAll('.operator');
-
 numbers.forEach((number) => {
     number.addEventListener('click', (event) => {
-        setNum1(event.target.value);
+        setNumber(event.target.value);
     });
 });
 
 operators.forEach((operator) => {
     operator.addEventListener('click', (event) => {
-        console.log(setOperation(event.target.value));
+        let number = setNumber(event.target.value);
+        getNumber(number);
+        calculator['operate'] = setOperation(event.target.value);
+        console.log(calculator['operate']);
+        console.log(calculator['num1']);
+        console.log(calculator['num2']);
     });
 });
+
+console.log(calculator['num1']);
+console.log(calculator['num2']);
+console.log(calculator['operate']);
